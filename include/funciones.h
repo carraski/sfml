ec75d074cd7aa0c5fad89cd7c98ffb20803cb5de
pinagof <pinagof@ieselaios.com>
2026-04-28 05:12:36 +0000

    arreglo error pinagof (funciones.h, funciones.cpp y main.cpp)

--- include/funciones.h
+++ include/funciones.h
@@ -67,7 +67,6 @@ public:
     void mover(sf::Vector2f direccion, float dt);
     void dibujar(sf::RenderWindow &ventana) const;
 
-
 private:
     sf::Texture textura;
     sf::Sprite sprite;
@@ -107,15 +106,15 @@ private:
     bool listo;
 };
 
-float modulo(sf::Vector2f vector){
+inline float modulo(sf::Vector2f vector)
+{
     return std::sqrt(vector.x * vector.x + vector.y * vector.y);
 }
 
-sf::Vector2f rotarVector(sf::Vector2f vector, float grados){
+inline sf::Vector2f rotarVector(sf::Vector2f vector, float grados)
+{
     float radianes = grados * 3.14159265f / 180.f;
     float cosA = std::cos(radianes);
     float sinA = std::sin(radianes);
     return sf::Vector2f(vector.x * cosA - vector.y * sinA, vector.x * sinA + vector.y * cosA);
 }
-
-
--- src/main.cpp
+++ src/main.cpp
@@ -57,7 +57,16 @@ int main()
 
 
         // Objeto Javier
-        // Movimiento con WASD
+        // Movimientfloat modulo(sf::Vector2f vector){
+    return std::sqrt(vector.x * vector.x + vector.y * vector.y);
+}
+
+sf::Vector2f rotarVector(sf::Vector2f vector, float grados){
+    float radianes = grados * 3.14159265f / 180.f;
+    float cosA = std::cos(radianes);
+    float sinA = std::sin(radianes);
+    return sf::Vector2f(vector.x * cosA - vector.y * sinA, vector.x * sinA + vector.y * cosA);
+}o con WASD
         sf::Vector2f dir(0.f, 0.f);
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
             dir.y -= 1.f;
